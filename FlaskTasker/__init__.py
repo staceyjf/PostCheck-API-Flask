639afac99dd1b71f20a9fe92f77b6fac1e48db5a
staceyjf <staceyfanner@gmail.com>
2024-06-24 20:42:41 +1000

    working mysql connection

--- FlaskTasker/__init__.py
+++ FlaskTasker/__init__.py
@@ -3,20 +3,22 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_cors import CORS
 
-db = SQLAlchemy()
 migrate = Migrate()
 cors = CORS()
 
+## import the entities
+from .models import Todo, Colour, db
+
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:MyPass1234!!@localhost/todo_flask'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password@localhost/todo_flask'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
     db.init_app(app)
     migrate.init_app(app, db)
     cors.init_app(app)
-
+    
     @app.route('/')
     def hello_world():
         return 'Hello, there World!'
